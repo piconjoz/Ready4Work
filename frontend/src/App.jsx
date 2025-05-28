26e526253db1b83be433b8ec7e6f89c84a33c2ce
Dinie Zikry Rudi <77114684+diniezikryy@users.noreply.github.com>
2025-05-28 17:32:51 +0800

    add(frontend-setup)
    frontend setup, with tailwindcss & react router for page routing

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import AppRoutes from "./routes.jsx";
+
+function App() {
+  return (
+    <>
+      <AppRoutes />
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/features/auth/pages/LogoutPage.jsx
@@ -0,0 +1,10 @@
+function LogoutPage() {
+  return (
+    <div>
+      <h1>Successfully Logged Out!</h1>
+      <p>Click here to login again</p>
+    </div>
+  );
+}
+
+export default LogoutPage;
